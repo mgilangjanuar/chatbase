f22f9690d7d013cb4cfa2da30c26bffdf44ac109
M Gilang Januar <mgilangjanuar@gmail.com>
2022-09-29 06:39:49 +0800

    fix #7

--- src/components/Chats/Main.tsx
+++ src/components/Chats/Main.tsx
@@ -44,18 +44,18 @@ export default function ({ toggle, user }: Props) {
   //   }
   // }, [])
 
-  useEffect(() => {
-    const x = () => {
-      const wrapper = document.querySelector('.chat-container-wrapper') as HTMLElement
-      if (wrapper) {
-        // wrapper.style.height = `${window.visualViewport?.height || window.innerHeight}px`
-        // wrapper.style.height = `${window.innerHeight}px`
-        wrapper.style.height = '100vh'
-      }
-    }
-    x()
-    // window.addEventListener('resize', x)
-  }, [])
+  // useEffect(() => {
+  //   const x = () => {
+  //     const wrapper = document.querySelector('.chat-container-wrapper') as HTMLElement
+  //     if (wrapper) {
+  //       // wrapper.style.height = `${window.visualViewport?.height || window.innerHeight}px`
+  //       // wrapper.style.height = `${window.innerHeight}px`
+  //       wrapper.style.height = '100vh'
+  //     }
+  //   }
+  //   x()
+  //   // window.addEventListener('resize', x)
+  // }, [])
 
   useEffect(() => {
     init()
